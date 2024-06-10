75b080ef4b5594fefb491c27d8a43c5ec449bdbf
Najmi Ariffin <booluckgmie@gmail.com>
2024-06-10 15:37:08 +0800

    Update csv_kb.py

--- csv_kb/csv_kb.py
+++ csv_kb/csv_kb.py
@@ -16,7 +16,7 @@ uploaded_file = st.file_uploader("Upload CSV file:")
 # Check if the file is uploaded
 if uploaded_file is not None:
     # Read the CSV file into a Pandas DataFrame
-    df = pd.read_csv(uploaded_file, low_memory=False)
+    df = pd.read_csv(uploaded_file, low_memory=False, encoding='latin-1')
 
     # Show the original DataFrame
     st.write("Original DataFrame:")
